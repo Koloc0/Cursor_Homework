83b03ad402d0961fb509409b6fed609138bd8472
Nykyta Udovychenko <nikitaudav123@gmail.com>
2022-10-24 22:44:55 +0300

    Initial commit

---
+++ src/com/cursor/homework1.java
@@ -0,0 +1,20 @@
+package com.cursor;
+
+public class homework1 {
+    public static void main(String[] args){
+
+        byte a = 4, b = 2;
+        short c = 12, d = 3;
+        long e = 90, f = 30;
+        int r = 200, v = 100;
+        double m = 4.4, n = 2.2;
+        float x = 20f, y = 10f;
+
+        System.out.println("Arithmetic operation byte: +=" + (a + b) + " -=" + (a - b) + " *=" + (a * b) + " /=" + (a/b));
+        System.out.println("Arithmetic operation short: +=" + (c + d) + " -=" + (c - d) + " *=" + (c * d) + " /=" + (c/d));
+        System.out.println("Arithmetic operation long: +=" + (e + f) + " -=" + (e - f) + " *=" + (e * f) + " /=" + (e/f));
+        System.out.println("Arithmetic operation int: +=" + (r + v) + " -=" + (r - v) + " *=" + (r * v) + " /=" + (r/v));
+        System.out.println("Arithmetic operation double: +=" + (m + n) + " -=" + (m - n) + " *=" + (m * n) + " /=" + (m/n));
+        System.out.println("Arithmetic operation float: +=" + (x + y) + " -=" + (x - y) + " *=" + (x * y) + " /=" + (x/y));
+    }
+}
